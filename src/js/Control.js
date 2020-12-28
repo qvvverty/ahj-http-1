dede62258c5913ebd139941c55cf6a556ce77544
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-12-28 20:07:48 +0300

    add some layout and controls

---
+++ src/js/Control.js
@@ -0,0 +1,50 @@
+export default class Control {
+  bindToDOM(parentEl) {
+    this.parentEl = parentEl;
+
+    this.modalBackground = document.querySelector('.modal-background');
+    this.createTicketForm = document.querySelector('[name="createTicket"]');
+    this.editTicketForm = document.querySelector('[name="editTicket"]');
+
+    document.querySelector('.add-ticket').addEventListener('click', this.addTicketBtnHandler.bind(this));
+    this.parentEl.addEventListener('click', this.cancelBtnHandler.bind(this));
+    document.addEventListener('click', this.ticketDescriptionHandler.bind(this));
+    this.parentEl.addEventListener('click', this.ticketEditHandler.bind(this));
+  }
+
+  addTicketBtnHandler() {
+    this.modalBackground.classList.remove('hidden');
+    this.createTicketForm.classList.remove('hidden');
+  }
+
+  cancelBtnHandler(click) {
+    if (click.target.classList.contains('cancel')) {
+      this.modalBackground.classList.add('hidden');
+      const parentForm = click.target.closest('form');
+      if (parentForm) {
+        parentForm.name.value = '';
+        parentForm.description.value = '';
+        parentForm.classList.add('hidden');
+        this.ticketToEditId = null;
+      }
+    }
+  }
+
+  ticketDescriptionHandler(click) {
+    if (click.target.classList.contains('ticket-name')) {
+      this.openedTicketDescription = click.target.closest('.ticket').querySelector('.ticket-description');
+      this.openedTicketDescription.classList.remove('hidden');
+    } else if (this.openedTicketDescription && click.target !== this.openedTicketDescription) {
+      this.openedTicketDescription.classList.add('hidden');
+      this.openedTicketDescription = null;
+    }
+  }
+
+  ticketEditHandler(click) {
+    if (click.target.classList.contains('ticket-edit')) {
+      this.ticketToEditId = click.target.closest('.ticket').dataset.id;
+      this.modalBackground.classList.remove('hidden');
+      this.editTicketForm.classList.remove('hidden');
+    }
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,4 @@
-const getTicketsForm = document.querySelector('[name="getTickets"]');
-const getTicketForm = document.querySelector('[name="getTicket"]');
-const createTicketForm = document.querySelector('[name="createTicket"]');
+import Control from './Control';
 
-getTicketsForm.addEventListener('submit', (submit) => {
-  submit.preventDefault();
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://localhost:7070/?method=allTickets');
-  xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === 4) {
-      console.log(xhr.response);
-    }
-  });
-  xhr.send();
-});
-
-getTicketForm.addEventListener('submit', (submit) => {
-  submit.preventDefault();
-  if (getTicketForm.id.value) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://localhost:7070/?method=ticketById&id=${getTicketForm.id.value}`);
-    xhr.addEventListener('readystatechange', () => {
-      if (xhr.readyState === 4) {
-        console.log(xhr.response);
-      }
-    });
-    xhr.send();
-
-    getTicketForm.id.value = '';
-  }
-});
-
-createTicketForm.addEventListener('submit', (submit) => {
-  submit.preventDefault();
-  if (createTicketForm.name.value && createTicketForm.description.value) {
-    const formData = new FormData(createTicketForm);
-    formData.set('status', createTicketForm.status.checked);
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:7070?method=createTicket');
-    xhr.send(formData);
-
-    createTicketForm.name.value = '';
-    createTicketForm.description.value = '';
-  }
-});
+const control = new Control();
+control.bindToDOM(document.querySelector('.helpdesk-container'));
